b0d745eb393c2d36e341d9c28650cc2ceb0788e3
Leonardo Castilho <leonardo.castilho@viannasempre.com.br>
2022-09-16 09:43:46 -0300

    Primeiro Array

--- bytebank_ATENDIMENTO/Program.cs
+++ bytebank_ATENDIMENTO/Program.cs
@@ -1,2 +1,26 @@
 ﻿Console.WriteLine("Boas Vindas ao ByteBank, Atendimento.");
 
+TestArrayInt();
+
+void TestArrayInt()
+{
+    int[] idades = new int[5];
+    idades[0] = 30;
+    idades[1] = 40;
+    idades[2] = 17;
+    idades[3] = 21;
+    idades[4] = 18;
+
+    Console.WriteLine($"Tamando do Aray: {idades.Length}");
+
+    int acumulador = 0;
+    for (int i = 0; i < idades.Length; i++)
+    {
+        int idade = idades[i];
+        acumulador = acumulador + idade;
+        Console.WriteLine($"Índice [{i}] = {idade}");
+    }
+
+    int media = acumulador / idades.Length;
+    Console.WriteLine($"A média é = {media}");
+}
